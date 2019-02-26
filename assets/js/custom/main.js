6432eea0c71e5eed8ab1482f2c3fb2987da712b4
Grigory Lobanov <yorgigiri@yandex.ru>
2019-02-26 15:51:32 +0400

    adaptive complete

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
 import 'slick-carousel';
+import 'magnific-popup';
 
 const reviewsAvatars = getReviewsAvatars();
 
@@ -23,7 +24,7 @@ $('#reviewsSlider').on('init', function (event, slick) {
 
     let dots = document.querySelector('.slick-dots').children;
 
-    for(let i = 0; i < dots.length; i++) {
+    for (let i = 0; i < dots.length; i++) {
         dots[i].style.background = 'url(' + reviewsAvatars[i] + ')';
         dots[i].style.backgroundSize = 'cover';
     }
@@ -32,5 +33,39 @@ $('#reviewsSlider').on('init', function (event, slick) {
 
 $('#reviewsSlider').slick({
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+                dots: true,
+                adaptiveHeight: true
+            }
+        }
+    ]
+});
+
+$(document).ready(function () {
+    $('#openTrueDetective').magnificPopup({
+        type: 'iframe',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: true,
+        callbacks: {
+            beforeOpen: function () {
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        },
+        zoom: {
+            enabled: true,
+            duration: 300,
+            easing: 'ease-in-out'
+        }
+    });
+});
+
+$('.header-menu__hamburger').on('click', function () {
+    $('.header-menu').toggleClass('header-menu_toggled');
+    $(this).toggleClass('header-menu__hamburger_menuIsVisible');
 });
